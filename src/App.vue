89ee924f6b3ba32e222528d416879e17752e3d5b
ren <renzhiwei@lewell.cn>
2019-11-06 10:52:23 +0800

    无token跳登录

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  created () {
+    const token = localStorage.getItem('token')
+    debugger
+    if (!token) {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
 <style>
 </style>
